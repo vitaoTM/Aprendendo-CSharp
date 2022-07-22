b7b020257b16e48f79fca35007086a4686288f45
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-22 20:08:41 -0300

    Programs update

--- Program.cs
+++ Program.cs
@@ -7,12 +7,14 @@ namespace Aprendendo_CSharp
         static void Main(string[] args)
         {
             using ContaCorrente;
-            ContaCorrente conta1 = new ContaCorrente();
-            conta1.titular = "Bruce Waine";
-            conta1.conta = "12345-6";
-            conta1.nome_agencia = "BatBank";
-            conta1.numero_agencia = 42;
-            conta1.saldo = 100000000000.99;
+            {
+                ContaCorrente conta1 = new ContaCorrente();
+                conta1.titular = "Bruce Waine";
+                conta1.conta = "12345-6";
+                conta1.nome_agencia = "BatBank";
+                conta1.numero_agencia = 42;
+                conta1.saldo = 100000000000.99;
+            }
 
 
             Console.WriteLine($"Titular: {conta1.titular}");
