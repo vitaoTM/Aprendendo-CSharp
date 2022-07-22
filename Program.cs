79ab0a86b4d02b9fc9223c3ee5172a86855cba10
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-22 16:14:45 -0300

    criando ambiente de trabalho C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aprendendo_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
