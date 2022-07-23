09030932a88ccbbb64b3066886ddd62516233976
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-23 10:10:37 -0300

    UPDATE file ContaCorrent

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -7,6 +7,50 @@ namespace Aprendendo_CSharp
         public string nome_agencia;
         public int numero_agencia;
         public double saldo;
+        public bool verificador;
 
+    }
+
+    public bool Sacar(double valor) 
+    {
+        if(saldo < valor)
+        {
+            return false;
+        }
+        if(valor < 0)
+        {
+            return false;
+        }
+        else 
+        {
+                saldo = saldo - valor;
+                return true;
+        }
+    }
+    public void Depositar(double valor)
+    {
+        if (valor < 0)
+        {
+            return;
+        }
+        saldo = saldo + valor;
+    }
+    public bool Transferir(double valor, ContaCorrente destino)
+    {
+        if (saldo < valor)
+        {
+            return false;
+        }
+        if (valor < 0)
+        {
+            return false;
+        }
+        else
+        {
+            saldo = saldo - valor;
+            destino.saldo = destino.saldo + valor;
+            return true;
+
+        }
     }
 }
